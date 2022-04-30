4d61b603e94cce898d823b985f6236dec62ea564
andsirson <104685071+andsirson@users.noreply.github.com>
2022-04-30 14:45:55 -0500

    Update stuff.py

--- stuff.py
+++ stuff.py
@@ -1,9 +1,10 @@
 import requests
 import json
 import xmltodict # will need to pip install xmltodict
-import csv
-import xml.etree.ElementTree as ET
-def getData(code):
-    return json.loads(json.dumps(xmltodict.parse(requests.get(f'https://api.globalgiving.org/api/public/projectservice/countries/{code}/projects?api_key=b65a4229-29bd-42ca-8267-eb7768a8110c').content)))['projects']['project']
+def getData():
+    return json.loads(json.dumps(xmltodict.parse(requests.get('https://api.globalgiving.org/api/public/projectservice/themes?api_key=b65a4229-29bd-42ca-8267-eb7768a8110c').content)))['themes']['theme']
 
-print(getData('in'))
+def get_thingy():
+    for i in getData():
+        print (f'<option value="{i["name"]}">{i["name"]}</option>')
+        
